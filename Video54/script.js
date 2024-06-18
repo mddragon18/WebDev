b01e59d17335137fac9f958020f2bf404934f7d5
mddragon18 <oveazmd@gmail.com>
2024-06-18 19:32:25 +0530

    .

---
+++ Video54/script.js
@@ -0,0 +1,7 @@
+console.log("Hello world");
+console.log("Code is running");
+console.log("THis is my first node js script");
+document.body.style.backgroundColor="orange";
+// document.div.style.backgroundColor="orange";
+
+
---
+++ Video56/main.js
@@ -0,0 +1,9 @@
+let num=9;
+if(num%2==0 || num%3==0)
+{
+    console.log("it is")
+}
+else
+{
+    console.log("It's not")
+}
